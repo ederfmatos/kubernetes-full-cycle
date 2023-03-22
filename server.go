f9f48e143e9f7603688905c9049360c8ba8f76f8
Eder Matos <ederfmatos@gmail.com>
2023-03-22 17:53:16 -0300

    secrets

--- server.go
+++ server.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/secrets", Secrets)
 	http.HandleFunc("/", Hello)
 	http.ListenAndServe(":8080", nil)
 }
@@ -16,4 +17,11 @@ func Hello(writer http.ResponseWriter, request *http.Request) {
 	age := os.Getenv("AGE")
 
 	fmt.Fprintf(writer, "Hello, I'm %s. I'm %s.", name, age)
+}
+
+func Secrets(writer http.ResponseWriter, request *http.Request) {
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+
+	fmt.Fprintf(writer, "User %s. Password %s.", user, password)
 }
